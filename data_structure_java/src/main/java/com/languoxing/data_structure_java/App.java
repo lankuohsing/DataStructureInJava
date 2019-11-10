b8fed9d736f4bfb443ab687d0da2dce4b3af61a8
lankuohsing <lankuohsing@163.com>
2019-11-11 00:21:45 +0800

    init a project. add class GameEntry

---
+++ data_structure_java/src/main/java/com/languoxing/data_structure_java/App.java
@@ -0,0 +1,13 @@
+package com.languoxing.data_structure_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ data_structure_java/src/main/java/com/languoxing/data_structure_java/array/GameEntry.java
@@ -0,0 +1,24 @@
+package com.languoxing.data_structure_java.array;
+
+public class GameEntry {
+    private String name;
+    private int score;
+
+    public GameEntry(String n, int s) {
+        name = n;
+        score = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + name +", " + score + ")";
+    }
+}
